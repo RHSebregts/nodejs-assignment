c60d72cd94dfd9ca1b88b6b96071d0101476441d
RHSebregts <mail@robbert-jan.com>
2021-12-01 14:34:47 +0100

    added incidint lookup

--- mongoServer/server.js
+++ mongoServer/server.js
@@ -14,7 +14,7 @@ mongoose.connect(dbLocation, async (err,res)=>{
         console.log('Succesfully connected to database')
         // Clean up collection if it gets too big
         const routeInformationCount = await RouteInformationEntry.count()
-        if(routeInformationCount > 40000){
+        if(routeInformationCount > 3000){
             RouteInformationEntry.collection.drop()
             IncidentEntry.collection.drop()
             console.log('Emptied the RouteInformation and Incident Collection')
@@ -26,19 +26,36 @@ mongoose.connect(dbLocation, async (err,res)=>{
 // REST Logic
 const express = require('express')
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 const http = require('http').createServer(app)
 const port = 2021
 http.listen(port, ()=>{
     console.log(`Listening on *:${port}`);
 });
-
 app.get("/", async(req,res)=>{
     res.render('../mongoServer/views/routeInformation.ejs')
 })
 app.get("/incidents", async(req,res)=>{
     const incidents = await IncidentEntry.find()
     res.render('../mongoServer/views/incidents.ejs', {incidents : incidents})
+})
+app.get("/incidentLookup", async (req,res)=>{
+    res.render('../mongoServer/views/incidentLookup.ejs', {incidents : null})
+})
+app.post("/findIncidents", async (req,res)=>{
+    const dbFilter = getRouteFilter(req.body, true)
+    const foundIncidents = await IncidentEntry.find(dbFilter).catch((err)=>{console.log(err)})
+    res.render('../mongoServer/views/incidents.ejs', {incidents : foundIncidents})
+})
+app.get("/routeInformationLookup", async (req,res)=>{
+    res.render('../mongoServer/views/routeInformationLookup.ejs', {routeEntries : null})
+})
+app.post("/findRouteEntries", async (req,res)=>{
+    const dbFilter = getRouteFilter(req.body)
+    const foundRouteInformation = await RouteInformationEntry.find(dbFilter).catch((err)=>{console.log(err)})
+    res.render('../mongoServer/views/viewRouteInformation.ejs', {routeEntries : foundRouteInformation})
 })
 // Websocket Logic
 const io = require('socket.io')(http);
@@ -48,7 +65,7 @@ io.on('connection', (socket) => {
     socket.emit('connectedToServer', true)
 })
 
-// Recieving and dealing with incoming data
+// Recieving and dealing with incoming data from NATS
 const NATS = require('nats')
 const nats = NATS.connect({json : true})
 
@@ -59,14 +76,47 @@ const listenForVehicleData = (vehicleName) => {
             let addNewEntry = await RouteInformationEntry.create(data)
             io.emit('vehicleMessage', data)
         }catch(err){
-           incidentHandling(err)
+           incidentHandling(err, data)
     }
     })
-  }
-
-const incidentHandling = (data) =>{
-    data.error = true
-    io.emit('vehicleError', data)
-    IncidentEntry.create({data : data})
 }
+
+const incidentHandling = (data, dataEntered) =>{
+    data.error = true
+    io.emit('vehicleError', dataEntered)
+    IncidentEntry.create({data : dataEntered})
+}
+const getRouteFilter = (routeCriteria, lookForIncident)=>{
+    let queryKeys = Object.keys(routeCriteria)
+    let dbFilter = {}
+    if(routeCriteria.time[0] !== 'ignore'){
+        let newDate = new Date(routeCriteria.time[1])
+        routeCriteria.time[1] = newDate.getTime()
+    }
+    queryKeys.forEach(key=>{
+        let queryItem = routeCriteria[key]
+        let dbKey = key
+        if(lookForIncident){
+            dbKey = `data.${key}`
+        }
+        if(queryItem[2]){
+            dbFilter[`data.${key}`] = {$eq : ''}
+        }else if(queryItem[0] === 'ignore'){
+            return
+        }else if(queryItem[0]=== 'gt'){
+            dbFilter[dbKey] = {$gt : parseFloat(queryItem[1])}
+        }else if(queryItem[0]=== 'lt'){
+            dbFilter[dbKey] = {$lt : parseFloat(queryItem[1])}
+        }else if(queryItem[0]=== '=='){
+            dbFilter[dbKey] = {$eq : parseFloat(queryItem[1])}
+        }
+    })
+    return dbFilter
+}
+
+// 
+//  =================================
+// 
+
 listenForVehicleData('test-bus-1')
+
